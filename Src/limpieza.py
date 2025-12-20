8cf218dafe7b279904e7df3db8a859031c2162b3
Angie <a.puerto@udea.edu.co>
2025-12-19 23:58:31 -0500

    Delete Src/limpieza.py

--- Src/limpieza.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def limpiar_datos(df):
-    df = df.copy()
-    df = df.drop_duplicates()
-    df['precio_por_tableta'] = pd.to_numeric(df['precio_por_tableta'], errors='coerce')
-    df = df.dropna(subset=['precio_por_tableta'])
-    return df
